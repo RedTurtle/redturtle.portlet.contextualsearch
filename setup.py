f3d480b65bda4f300600cab2d444ee2aec0f2804
micecchi <micecchi@redturtle.it>
2011-07-11 12:55:28 +0000

    fixed version

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup, find_packages
 import os
 
-version = '1.2.2'
+version = '1.2.3'
 
 setup(name='redturtle.portlet.contextualsearch',
       version=version,
-      description="",
+      description="A contextual search portlet",
       long_description=open("README.txt").read() + "\n" +
                        open(os.path.join("docs", "HISTORY.txt")).read(),
       # Get more strings from http://www.python.org/pypi?%3Aaction=list_classifiers
